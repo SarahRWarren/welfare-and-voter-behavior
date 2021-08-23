514e8143156017bbf521c0f2c7527c92cc5f2ea4
SarahRWarren <54514764+SarahRWarren@users.noreply.github.com>
2021-08-23 10:52:18 -0400

    drafting

--- ext.R
+++ ext.R
@@ -47,3 +47,33 @@ ext9 <- lm(PEOPSAY ~ white + EDUC + sex + PTYIDd +
 stargazer(ext9,
           title="Effect of Aid on Self-Efficacy", type="latex", style = "apsr",
           align=TRUE, out="Tables/ext2.tex")
+
+tex1 <- glm(PEOPSAY ~ aid_dummy + PTYIDd + white + EDUC + sex + 
+             FOLLOWPA + TRUSTOFF,
+          weights = allwt, data =  max_sub, family="binomial")
+tex2 <- glm(PEOPSAY ~ aid_dummy + PTYIDd + aid_dummy*PTYIDd + white + EDUC + sex + 
+             FOLLOWPA + TRUSTOFF,
+          weights = allwt, data =  max_sub, family="binomial")
+tex3 <- glm(PEOPSAY ~ Total + PTYIDd + white + EDUC + sex + 
+             FOLLOWPA + TRUSTOFF,
+          weights = allwt, data =  max_sub, family="binomial")
+
+tex4 <- glm(PEOPSAY ~ total_uni + PTYIDd + 
+             white + EDUC + sex + FOLLOWPA + TRUSTOFF,
+          weights = allwt, data =  max_sub, family="binomial")
+
+tex5 <- glm(PEOPSAY ~ total_loans + PTYIDd + 
+             white + EDUC + sex + FOLLOWPA + TRUSTOFF,
+          weights = allwt, data =  max_sub, family="binomial")
+
+tex6 <- glm(PEOPSAY ~  total_ent + PTYIDd + 
+             white + EDUC + sex + FOLLOWPA + TRUSTOFF,
+          weights = allwt, data =  max_sub, family="binomial")
+
+tex7 <- glm(PEOPSAY ~  total_means + PTYIDd + 
+             white + EDUC + sex + FOLLOWPA + TRUSTOFF,
+          weights = allwt, data =  max_sub, family="binomial")
+
+stargazer(tex1, tex2, tex3, tex4, tex5, tex6, tex7,
+          title="Effect of Aid on Self-Efficacy", type="latex", style = "apsr",
+          align=TRUE, out="Tables/tab2.tex")
